972ef75bfd39de0a4abe5693e0d85c58685e18e1
Clokwerk <35271524+Clokwerk@users.noreply.github.com>
2020-07-31 06:50:46 -0700

    Primarni modeli
    Dodadeni primarni modeli za Prodavnica, Proizvod i Kategorija.

---
+++ CobaltElectronics/Models/Kategorija.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CobaltElectronics.Models
+{
+    public class Kategorija
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public List<Proizvod> proizvodi { get; set; }
+        public Kategorija()
+        {
+            proizvodi=new List<Proizvod>();
+        }
+
+
+    }
+}
---
+++ CobaltElectronics/Models/Proizvod.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CobaltElectronics.Models
+{
+    public class Proizvod
+    {
+        [Required]
+        public int Id { get; set; }
+        public string Ime { get; set; }
+        public float Cena { get; set; }
+        public List<Prodavnica> prodavnici {get;set;}
+
+        public Proizvod()
+        {
+            prodavnici = new List<Prodavnica>();
+        }
+
+
+    }
+}
